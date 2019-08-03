78988ce0cb78984cc8fa198ad2e01ee04ed1220c
Steve Singh <steve.a.singh@gmail.com>
2019-08-03 13:30:19 -0400

    Updates

---
+++ precache-manifest.63a28125e3d8bbd4e5ab6cb52ade7518.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb4f3f6d582587c29ec15d58b3b0e70e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "21b72ab23a4384396738",
+    "url": "/monsters-rolodex/static/css/main.028c2d23.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "21b72ab23a4384396738",
+    "url": "/monsters-rolodex/static/js/main.2aceeddb.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
